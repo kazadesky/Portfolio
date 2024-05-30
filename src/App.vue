b821727927b1238f49045540a39478b1472c893a
kazadesky <kazadesky123@gmail.com>
2024-05-30 15:00:57 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <LandingPage />
+  </div>
+</template>
+
+<script>
+import LandingPage from '@/pages/LandingPage.vue';
+
+export default {
+  name: 'app-component',
+  components: {
+    LandingPage,
+  }
+}
+</script>
